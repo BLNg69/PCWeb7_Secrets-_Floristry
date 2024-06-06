f2b18cef736ea971f109af6225fc6070fb9155d8
Ng Boon Leng <Ng_Boon_Leng@yahoo.com>
2024-06-06 12:10:15 +0800

    Change the BG color of Nav bar to "Purple"

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,8 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 export default function Navigation() {
   const isHomePage = window.location.pathname === '/';
   return (
-    <Navbar variant="dark" bg="dark">
+    // <Navbar variant="dark" bg="dark">
+    <Navbar variant="dark" style={{ backgroundColor: '#6A5ACD' }}>
       <Container>
       <Navbar.Brand href="/">Secret Floristry</Navbar.Brand>
         <Nav>
